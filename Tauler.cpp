c0e9a7f95aba61bdd5f7914a51aebae3426463eb
NIU1708307 <149054066+NIU1708307@users.noreply.github.com>
2024-04-30 20:37:33 +0200

    commit "tauler"

--- Tauler.cpp
+++ Tauler.cpp
@@ -8,3 +8,69 @@ Tauler::Tauler()
 			tauler[i][j] = NO_COLOR;
 		}
 }
+
+void setTauler(ColorFigura color, int x, int y)
+{
+			tauler[x][y] = color;	
+}
+
+
+bool Tauler::casellaOcupada(ColorFigura tauler)
+{
+	bool Ocupada;
+	if (tauler.getTauler() = NO_COLOR)
+		Ocupada = true;
+	else
+		Ocupada = false;
+
+	return Ocupada;
+}
+
+bool Tauler::filaPlena(int fila)
+{
+	for (int col = 0;col < MAX_COL;i++)
+	{
+		if (tauler.getTauler(fila, col) == NO_COLOR)
+			return false;
+	}
+	return true;
+}
+
+void Tauler::eliminaFila(int fila)
+{
+
+	for (int col = 0; col < MAX_COL; col++)
+	{
+		setTauler(0,fila,col);
+	}
+
+}
+
+void Tauler::baixaFila(int fila, int nFiles)
+{
+	for (int col = 0; col < MAX_COL; col++)
+	{
+		setTauler(tauler[fila][col], fila + nFiles, col);
+		setTauler(0, fila, col);
+	}
+
+}
+
+int Tauler::borraFilaPlena()
+{
+	int Ple = 0;
+	for (int i = nfiles - 1;i >= 0; fila--)
+	{
+		if (filaPlena(i))
+		{
+			eliminaFila(fila);
+			Ple++;
+		}
+		else
+			if (ple > 0)
+			{
+				baixafila(i,Ple)
+			}
+	}
+	return Ple;
+}
--- Tauler.h
+++ Tauler.h
@@ -10,12 +10,17 @@ class Tauler
 public:
 	Tauler();
 	
-	void setTauler(ColorFigura n_tauler[MAX_FILA][MAX_COL]) { tauler = n_tauler; }
-	ColorFigura getTauler() const { return tauler; }
-
+	void setTauler(ColorFigura n_tauler[MAX_FILA][MAX_COL]);
+	ColorFigura getTauler(int x, int y) const { return tauler[x][y]; }
+	bool casellaOcupada(ColorFigura tauler);
+	void eliminaFila(int fila);
+	bool filaPlena(int fila);
+	void baixaFila(int fila,int nFiles);
+	int borraFilaPlena()
 private:
 	ColorFigura tauler[MAX_FILA][MAX_COL];
 	Figura figuraActual;
+	
 
 };
 #endif
