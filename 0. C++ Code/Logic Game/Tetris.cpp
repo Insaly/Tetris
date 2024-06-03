f6df148e80f48fb639dc4718cf5f999232441b23
NIU1708307 <149054066+NIU1708307@users.noreply.github.com>
2024-06-03 19:43:49 +0200

    puntuacions

--- 0. C++ Code/Logic Game/Tetris.cpp
+++ 0. C++ Code/Logic Game/Tetris.cpp
@@ -1,4 +1,5 @@
 #include "./Tetris.h"
+#include <forward_list>
 
 int Tetris::mostraMenu()
 {
@@ -13,9 +14,67 @@ int Tetris::mostraMenu()
     return opcio;
 }
 
-void Tetris::mostraPuntuacions()
+void Tetris::mostraPuntuacions(const string& nomFitxer)
 {
-    string nomFitxer;
-    cout << "Introdueix fitxer de puntuacions: ";
-    cin >> nomFitxer;
+    ifstream fitxer;
+
+    fitxer.open(nomFitxer);
+    int x;
+    cout << "Llista millors puntuacions" << endl;
+    cout << "==========================" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        fitxer >> x;
+        cout << i + 1 << " " << x << endl;
+    }
+
+}
+
+void Tetris::afegeixPuntuacio(int puntuacio, const string& nomFitxer)
+{
+    int i = 0;
+    ifstream fitxer;
+    fitxer.open(nomFitxer);
+
+    if (fitxer.is_open())
+    {
+        int x;
+        
+        while (!fitxer.eof() )
+        {
+            fitxer >> x;
+            puntuacions.push_front(x);   
+        }
+        fitxer.close();
+    }
+    puntuacions.push_front(puntuacio);
+
+    puntuacions.sort(std::greater <int>());
+
+    while (i <= 10)
+    {
+        it = puntuacions.begin();
+     
+        if (i == 10)
+        {
+            puntuacions.erase_after(it);
+        }
+        i++;
+        it++;
+    }
+
+    ofstream fitxer1;
+
+    fitxer1.open(nomFitxer);
+
+    if (fitxer.is_open())
+    {
+        for (it=puntuacions.begin(); it != puntuacions.end(); it++)
+        {
+            int puntuacio = *it;
+            fitxer1 << puntuacio << endl;
+
+        }
+        fitxer.close();
+    }
 }
--- 0. C++ Code/Logic Game/Tetris.h
+++ 0. C++ Code/Logic Game/Tetris.h
@@ -3,18 +3,22 @@
 
 #include "./Partida.h"
 #include <iostream>
+#include <forward_list>
 
 class Tetris
 {
 public:
     int mostraMenu();
-    void mostraPuntuacions();
+    void mostraPuntuacions(const string& nomFitxer);
     void jugaPartida(double deltaTime) { m_partida.actualitza(deltaTime); }
     void inicialitzaPartida(int mode, const string& fitxerPartida, const string& fitxerFigures, const string& fitxerMoviments) { m_partida.inicialitza(mode, fitxerPartida, fitxerFigures, fitxerMoviments); }	
-    int getPuntuacio() { return puntuacio; }
+    int getPuntuacio() { return puntuacions; }
+    void afegeixPuntuacio(int puntuacio, const string& nomFitxer);
 private:
     Partida m_partida;
-    int puntuacio;
+    int puntuacions;
+    std::forward_list <int> puntuacions;
+    std::forward_list <int> :: iterator actual,it;
 };
 
 #endif 
